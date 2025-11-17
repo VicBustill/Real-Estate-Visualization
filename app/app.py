ff880aaf55b864aad1f85825a50147a18c43799d
Victor <149431549+VicBustill@users.noreply.github.com>
2025-11-17 20:01:23 +0000

    I added some parameters for our filters (Later to be used potentially for API)

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import pandas as pd  # type: ignore
 import pydeck as pdk  # type: ignore
 from utils.io import load_first_csv
 
+
 st.set_page_config(page_title="CSC 481 Real Estate Dashboard", layout="wide")
 st.title("Real Estate Visualization (CSC 481)")
 st.caption(
@@ -16,12 +17,21 @@ st.write("DEBUG: DataFrame type ->", type(df))
 
 with st.sidebar:
     st.header("Filters")
+    # One of the parameters for our API 1/3
     zip_code = st.text_input("ZIP (optional)", "")
+    # One of the parameters for our API 2/3
+    state = st.text_input("Abbreviated State", "")
+    city = st.text_input("City", "")  # One of the parameters for our API 3/3
     min_price = st.number_input("Min price", min_value=0, value=0, step=50000)
     max_price = st.number_input(
         "Max price", min_value=0, value=2_000_000, step=50000)
     min_beds = st.number_input("Min beds", min_value=0, value=0, step=1)
+    max_beds = st.number_input("Min beds", min_value=0, value=0, step=1)
 
+# ______________This section will make the API call ________________________
+
+
+# _________________________________________________________________________
 # if df is None:
     # st.info("No data yet. Add a CSV into the `data/` folder and reload.")
 # else:
